6f7943c955307d3ce12c75e114a3bb7bfa6c22cd
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-01-03 13:56:37 +0530

    incrementBy5 and decrementBy5 added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,6 +13,13 @@ const decrementHandler=()=>{
   dispatch({type:'decrement'})
 }
 
+const incrementBy5Handler=()=>{
+  dispatch({type:'incrementBy5'})
+}
+
+const decrementBy5Handler=()=>{
+  dispatch({type:'decrementBy5'})
+}
   const toggleCounterHandler = () => {};
 
   return (
@@ -22,6 +29,8 @@ const decrementHandler=()=>{
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementBy5Handler}>IncrementBy5</button>
+        <button onClick={decrementBy5Handler}>DecrementBy5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- src/store/index.js
+++ src/store/index.js
@@ -11,6 +11,18 @@ const counterReducer = (state = { counter: 0 }, action) => {
       counter: state.counter - 1,
     };
   }
+
+  if (action.type === "incrementBy5") {
+    return {
+      counter: state.counter +5 ,
+    };
+  }
+
+  if (action.type === "decrementBy5") {
+    return {
+      counter: state.counter - 5,
+    };
+  }
   return state;
 };
 
