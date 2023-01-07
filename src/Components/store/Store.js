3c7032936a69606806a1ba40a440d08833501e64
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-01-07 15:44:21 +0530

    Delete src/Components directory

--- src/Components/store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import AuthReducer from "./AuthReducer";
-import ExpenseReducer from "./ExpenseReducer";
-
-const store = configureStore({
-  reducer: { auth: AuthReducer, expense: ExpenseReducer },
-});
-
-export default store;
